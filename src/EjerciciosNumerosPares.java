9b83a994b8d6d14fa4fdbd7c6bdebf204be4cbe9
Arturo Valdez <santiguardado27@gmail.com>
2025-01-09 19:51:19 -0600

    Ejercicio Numeros Pares

--- src/EjerciciosNumerosPares.java
+++ src/EjerciciosNumerosPares.java
@@ -2,6 +2,7 @@ public class EjerciciosNumerosPares {
     public static void main(String[] args) {
         System.out.println("*** Numeros Pares ***");
 
+        //Uso de while interar pares
         var contador = 1;
         while (contador<=20){
             if(contador % 2 == 0)
