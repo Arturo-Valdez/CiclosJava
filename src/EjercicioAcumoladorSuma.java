23b323102cde8baca4a0f551014c583cd4e95fa7
Arturo Valdez <santiguardado27@gmail.com>
2025-01-09 20:31:46 -0600

    Uso Acumulador Suma Mejorada

--- src/EjercicioAcumoladorSuma.java
+++ src/EjercicioAcumoladorSuma.java
@@ -2,13 +2,23 @@ public class EjercicioAcumoladorSuma {
     public static void main(String[] args) {
         System.out.println("*** Acumolador Suma ***");
 
+        //Maximo a contar
         final var MAXIMO = 5;
-        var contar = 0;
+        //Contador
+        var acumuladorSuma = 0;
+        //Acumulador de suma
         var contador = 1;
-        while (contador <= MAXIMO){
-            contar = contar + contador;
-            contador++;
+
+        while (contador <= MAXIMO) {
+            System.out.println("acumuladorSuma + contador -> " + acumuladorSuma + " + " + contador);
+
+            //Se realiza acumulacion
+            acumuladorSuma += contador++;
+
+            System.out.println("Suma parcial acumulada: " + acumuladorSuma + "\n");
+
         }
-        System.out.println("Suma de los primeros "+MAXIMO + " numeros es igual a: "+ contar);
+        //Se imprime la acumulacion de suma
+        System.out.println("Suma de los primeros "+MAXIMO + " numeros es igual a: "+ acumuladorSuma);
     }
 }
