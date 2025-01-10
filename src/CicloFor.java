1145cdf3abcc768bf4120af8d7dc07927a124592
Arturo Valdez <santiguardado27@gmail.com>
2025-01-09 20:15:57 -0600

    Uso For

---
+++ src/CicloFor.java
@@ -0,0 +1,11 @@
+public class CicloFor {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclo For ***");
+
+        for(var i = 1; i<= 50; i++){
+            if(i  == 20 || i == 36)
+                System.out.println();
+            System.out.print(i + " ");
+        }
+    }
+}
