7cf3e2cb4ab03bd69abf5234559157d69c6a1a20
Arturo Valdez <santiguardado27@gmail.com>
2025-01-09 19:44:43 -0600

    Ciclos en JAVA

---
+++ src/CicloWhile.java
@@ -0,0 +1,11 @@
+public class CicloWhile {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclos while ***");
+
+        var contador = 1;
+        while (contador <= 5){
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
